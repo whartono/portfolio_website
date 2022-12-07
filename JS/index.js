63243bdf72f533a1ca7348acda774dabc9c719c9
William Hartono <whartono2018@gmail.com>
2022-12-06 20:53:44 -0800

    Testing Github Connection.

---
+++ JS/index.js
@@ -0,0 +1,59 @@
+/*
+We use 'const' for values that don't change. 'Let' is used for values that are to be reassigned. 
+
+Also, delay values are in ms
+*/
+
+const typedTextSpan = document.querySelector('.typed-text')
+const cursorSpan = document.querySelector('.cursor');
+const twText = ['Software Engineer', 'Database Administrator', 'Freelance Web Developer', 'Student']
+const typingDelay = 200;
+const erasingDelay = 100;
+const nextStringDelay = 3000;
+
+let twIndex = 0;
+let charIndex = 0;
+
+
+function type() {
+    if (charIndex < twText[twIndex].length) {
+        if (!cursorSpan.classList.contains('typing')) cursorSpan.classList.add('typing');
+
+        typedTextSpan.textContent += twText[twIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    }
+    else {
+        cursorSpan.classList.remove('typing');
+        setTimeout(erase, nextStringDelay);
+    }
+}
+
+function erase() {
+    if (charIndex > 0) {
+        if (!cursorSpan.classList.contains('typing')) cursorSpan.classList.add('typing');
+
+        typedTextSpan.textContent = twText[twIndex].substring(0, charIndex-1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    }
+    else {
+        twIndex++;
+
+        if (twIndex >= twText.length) {
+            twIndex = 0;
+        }
+
+        cursorSpan.classList.remove('typing');
+        setTimeout(type, typingDelay + 1100);
+    }
+}
+
+
+//Works automatically when the content is loaded
+document.addEventListener('DOMContentLoaded', function() {
+
+    setTimeout(type, nextStringDelay+250);
+});
+
+
---
+++ JS/nav.js
@@ -0,0 +1,25 @@
+
+window.addEventListener('load', function() {
+    console.log("HELLO WORLD");
+    
+    gsap.fromTo('.sidebar-content .picture', 
+        {opacity: 0, y:-10},
+        {opacity: 1, y:0, delay: 0.25}
+    );
+
+    gsap.fromTo('.sidebar-content h2', 
+        {opacity: 0, y:-10},
+        {opacity: 1, y:0, delay: 0.25}
+    );
+
+
+    gsap.fromTo('.sidebar-content ul li', 
+        {opacity: 0, y:-10},
+        {opacity: 1, y:0, delay: 0.25, stagger:0.25}
+    );
+
+    gsap.fromTo('.sidebar-content .socials', 
+        {opacity: 0, y:-10},
+        {opacity: 1, y:0, delay: 1.75}
+    );
+});
